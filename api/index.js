3713b963900d5a3b95459f2a2f80cf5b8d0dad6b
Marcos Bermejo <marcos.bermejo@vlex.com>
2024-04-05 00:03:50 +0200

    fix: cors

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
 const locations = require('./data');
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get('/:num?', function (req, res, next) {
 
