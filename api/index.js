4f3f517a9c24e7e94316eec4f6122387e758969f
Marcos Bermejo <marcos.bermejo@vlex.com>
2024-04-04 23:57:10 +0200

    clean up

---
+++ api/index.js
@@ -0,0 +1,21 @@
+const locations = require('./data');
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.get('/:num?', function (req, res, next) {
+
+  let num = Math.max(0, parseInt(req.params.num || 0));
+  num = num % 21;
+
+  res.json({
+    location: {
+      a: locations[0].slice(0, num),
+      b: locations[1].slice(0, num)
+    }
+  })
+});
+
+app.listen(3001, () => console.log('Server ready on port 3001.'));
+
+module.exports = app;
